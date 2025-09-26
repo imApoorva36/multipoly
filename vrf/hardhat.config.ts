c946ccfb62325b0359d2c199ad5ca526701f4565
VedantTarale <vedanttarale10@gmail.com>
2025-09-26 23:34:56 +0530

    [VRF]: Setup hardhat

--- vrf/hardhat.config.ts
+++ vrf/hardhat.config.ts
@@ -1,8 +1,17 @@
-import type { HardhatUserConfig } from "hardhat/config";
+import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import * as dotenv from "dotenv";
+
+dotenv.config();
 
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
+  networks: {
+    flow_evm: {
+      url: process.env.FLOW_EVM_RPC_URL || "",
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+    }
+  },
 };
 
 export default config;
